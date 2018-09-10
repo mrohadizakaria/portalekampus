4c3000580aa07d80e39a0ceb5e4b59ace4e98a08
MOCHAMMAD RIZKI ROMDONI <cv.yacanet@gmail.com>
2018-09-10 13:42:55 +0700

    Ganti Method yang deprecated; mengakibatkan nilai total bayar mahasiswa lebih besar

--- protected/pages/k/limitless/pembayaran/DetailPembayaranSemesterPendek.php
+++ protected/pages/k/limitless/pembayaran/DetailPembayaranSemesterPendek.php
@@ -5,15 +5,16 @@ class DetailPembayaranSemesterPendek Extends CDetailPembayaranSemesterPendek {
 		parent::onLoad($param);							
     }
     public function checkPembayaranSemesterLalu () { 
-        $datamhs=$_SESSION['currentPagePembayaranSemesterPendek']['DataMHS'];	
-		$ta=$datamhs['ta'];			
+        $datamhs=$_SESSION['currentPagePembayaranSemesterPendek']['DataMHS'];			
+        $ta=$datamhs['ta'];			
         $semester=2;
-            
+        $datamhs['idsmt']=$semester;
+        
         $idkelas=$this->Finance->getKelasFromTransaksi($ta,$semester);
         $datamhs['idkelas']=$idkelas===false?$datamhs['idkelas']:$idkelas;
         $this->Finance->setDataMHS($datamhs);
         if ($idkelas!='C') {
-            $totalbiaya=$this->Finance->getTotalBiayaMhs ('lama');
+            $totalbiaya=$this->Finance->getTotalBiayaMhsPeriodePembayaran ('lama');
             $totalbayar=$this->Finance->getTotalBayarMhs($ta,$semester);
             $sisa=$totalbiaya-$totalbayar;
 
