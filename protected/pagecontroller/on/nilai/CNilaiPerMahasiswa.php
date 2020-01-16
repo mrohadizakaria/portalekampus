8bb23420ad05c8cb2179b8f4280a9ec32e79def8
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-01-16 09:16:15 +0700

    tambah autonumeric pada input nilai

--- protected/pagecontroller/on/nilai/CNilaiPerMahasiswa.php
+++ protected/pagecontroller/on/nilai/CNilaiPerMahasiswa.php
@@ -14,8 +14,8 @@ class CNilaiPerMahasiswa extends MainPageON {
 			}
             $nim=addslashes($this->request['id']);       
             try {     
-                $str = "SELECT vdm.no_formulir,vdm.nim,vdm.nirm,vdm.nama_mhs,vdm.jk,vdm.tempat_lahir,vdm.tanggal_lahir,vdm.kjur,vdm.nama_ps,vdm.idkonsentrasi,k.nama_konsentrasi,vdm.tahun_masuk,iddosen_wali,vdm.k_status,sm.n_status AS status,vdm.idkelas,ke.nkelas  FROM v_datamhs vdm LEFT JOIN konsentrasi k ON (vdm.idkonsentrasi=k.idkonsentrasi) LEFT JOIN status_mhs sm ON (vdm.k_status=sm.k_status) LEFT JOIN kelas ke ON (vdm.idkelas=ke.idkelas) WHERE vdm.nim='$nim'";
-                $this->DB->setFieldTable(array('no_formulir','nim','nirm','nama_mhs','jk','tempat_lahir','tanggal_lahir','kjur','nama_ps','idkonsentrasi','nama_konsentrasi','tahun_masuk','iddosen_wali','k_status','status','idkelas','nkelas'));
+                $str = "SELECT vdm.no_formulir,vdm.nim,vdm.nirm,vdm.nama_mhs,vdm.jk,vdm.tempat_lahir,vdm.tanggal_lahir,vdm.kjur,vdm.nama_ps,vdm.idkonsentrasi,k.nama_konsentrasi,vdm.tahun_masuk,iddosen_wali,vdm.k_status,sm.n_status AS status,vdm.idkelas,ke.nkelas,photo_profile FROM v_datamhs vdm LEFT JOIN konsentrasi k ON (vdm.idkonsentrasi=k.idkonsentrasi) LEFT JOIN status_mhs sm ON (vdm.k_status=sm.k_status) LEFT JOIN kelas ke ON (vdm.idkelas=ke.idkelas) WHERE vdm.nim='$nim'";
+                $this->DB->setFieldTable(array('no_formulir','nim','nirm','nama_mhs','jk','tempat_lahir','tanggal_lahir','kjur','nama_ps','idkonsentrasi','nama_konsentrasi','tahun_masuk','iddosen_wali','k_status','status','idkelas','nkelas','photo_profile'));
                 $r=$this->DB->getRecord($str);	           
                 if (!isset($r[1])) {
                     unset($_SESSION['currentPageNilaiPerMahasiswa']);
@@ -25,6 +25,7 @@ class CNilaiPerMahasiswa extends MainPageON {
                             </div>");
                 }
                 $datamhs=$r[1];
+                $this->Nilai->setDatamHS($datamhs);
                 $datamhs['iddata_konversi']=$this->Nilai->isMhsPindahan($nim,true);
                 
                 $kelas=$this->Nilai->getKelasMhs();                
