8ba75c948bdece3afff175a61833ff26f9df83d3
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-01-20 07:39:18 +0700

    Pembayaran Piutang Tidak Tertagih

---
+++ protected/pages/k/limitless/report/PembayaranPiutangTidakTertagihGanjil.php
@@ -0,0 +1,7 @@
+<?php
+prado::using ('Application.pagecontroller.k.report.CPembayaranPiutangTidakTertagihGanjil');
+class PembayaranPiutangTidakTertagihGanjil extends CPembayaranPiutangTidakTertagihGanjil {
+	public function onLoad($param) {		
+		parent::onLoad($param);						      
+	}    
+}
