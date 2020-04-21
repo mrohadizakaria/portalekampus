55e7bb52bd54941603ae0687ce0a2fd98ddbd1f6
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-04-21 23:34:47 +0700

    auth 3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,19 @@
 <template>
 	<v-app>
-
+		<router-view/>		
+		<v-snackbar v-model="snackbar_success" :color="snackbar_color" :top="true">
+			{{ page_message }}<br>			
+		</v-snackbar>
+		<v-snackbar v-model="snackbar_error" :color="snackbar_color" :top="true">
+			{{ page_message }}<br>
+			<div v-for="err in page_form_error_message" v-bind:key="err.name">
+				<strong>{{err.field}}</strong>
+				<div v-for="error in err.error" v-bind:key="error.message">
+					{{error.message}}
+				</div>
+			</div>
+		</v-snackbar>
+		<confirm ref="confirm"></confirm>
 	</v-app>
 </template>
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import moment from 'moment';
+import api from './plugins/api';
+
+Vue.use(api);
 
 Vue.config.productionTip = false
 
+//filter output
+Vue.filter('formatTanggal', function(value) 
+{
+	var tanggal = moment(String(value)).format('DD/MM/YYYY hh:mm');    
+    return tanggal;
+});
+Vue.filter('formatUang', function(value) 
+{
+	var num = new Number(value).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1.');
+	var pos = num.lastIndexOf('.');
+	num = num.substring(0,pos) + ',' + num.substring(pos+1)	
+	return num;
+});
+
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+	router,
+	store,
+	vuetify,
+	render: h => h(App)
 }).$mount('#app')
