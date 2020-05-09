12f2e3ed019f3648351653483a478d8e6446f4ab
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-05-09 07:42:56 +0700

    struckture db 1

--- portalekampus.sql
+++ portalekampus.sql
@@ -1415,7 +1415,7 @@ INSERT INTO `setting` (`setting_id`, `group`, `key`, `value`) VALUES
 (6, 'general', 'default_kjur', '1'),
 (60, 'krs', 'jumlah_sks_krs_setelah_cuti', '12'),
 (61, 'krs', 'jumlah_sks_krs_mhs_baru', '21'),
-(56, 'spmb', 'default_tahun_pendaftaran', '2018'),
+(56, 'spmb', 'default_tahun_pendaftaran', '2018');
 
 --
 -- Table structure for table `soal`
