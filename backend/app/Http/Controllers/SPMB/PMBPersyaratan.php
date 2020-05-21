333122c1024fa17a9793b2eebb130ba3cd988e49
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-05-21 07:05:45 +0700

    upload persyaratan 1

---
+++ backend/app/Http/Controllers/SPMB/PMBPersyaratan.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\SPMB;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+use Ramsey\Uuid\Uuid;
+
+class PMBPersyaratanController extends Controller { 
+    
+}
--- frontend/src/components/FormPersyaratanPMB.vue
+++ frontend/src/components/FormPersyaratanPMB.vue
@@ -1,21 +1,40 @@
 <template>
-    <v-row no-gutters>
-        <v-col cols="12">
-            <v-form ref="frmdata" v-model="form_valid" lazy-validation>
-                <v-card class="mb-4">
-                    <v-card-text>
-                        <v-select
-                            label="JENIS PERSYARATAN"
-                            :items="daftar_persyaratan"
-                            v-model="persyaratan_id"
-                            item-text="nama_persyaratan"
-                            item-value="id"                                                        
-                            filled
-                        />
+    <v-row>        
+        <v-col cols="4" v-for="(item,index) in daftar_persyaratan" v-bind:key="item.id">
+            <v-form v-model="form_valid" ref="frmpersyaratan" lazy-validation>
+                <v-card class="mx-auto" max-width="400">
+                    <v-img class="white--text align-end" height="200px" :src="require('@/assets/no-image.png')"></v-img>                
+                    <v-card-text class="text--primary">
+                        <div>{{index}}
+                            <v-file-input 
+                                accept="image/jpeg,image/png" 
+                                :label="item.nama_persyaratan+' (.png atau .jpg)'"
+                                :rules="rule_foto"
+                                show-size
+                                v-model="formdata.foto"
+                                @change="previewImage">
+                            </v-file-input>
+                        </div>
                     </v-card-text>
-                </v-card>
+                    <v-card-actions>
+                        <v-btn
+                            color="orange"
+                            text
+                            @click="upload(index)"
+                        >
+                            Upload
+                        </v-btn>
+
+                        <v-btn
+                            color="orange"
+                            text
+                        >
+                            Hapus
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>            
             </v-form>
-        </v-col>
+        </v-col>        
     </v-row>
 </template>
 <script>
@@ -29,13 +48,15 @@ export default {
     data:()=>({
         //form
         form_valid:true,
-
         daftar_persyaratan:[],
-        persyaratan_id:'',        
-
         formdata:{
-            nama_persyaratan:'',            
+            foto:'',            
         },
+        //form rules  
+        rule_foto:[
+            value => !!value||"Mohon pilih gambar !!!",  
+            value =>  !value || value.size < 2000000 || 'File foto harus kurang dari 2MB.'                
+        ], 
 
     }),
     methods: {
@@ -48,6 +69,14 @@ export default {
             ).then(({data})=>{   
                 this.daftar_persyaratan=data.persyaratan;
             })
+        },
+        previewImage (e)
+        {
+            console.log(e);
+        },
+        upload(index)
+        {
+            console.log(index);
         }
     },
     computed:{
